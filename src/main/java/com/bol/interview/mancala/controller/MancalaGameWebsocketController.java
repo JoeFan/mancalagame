263586bd74c83dc640cc746a0c1c8039034ac050
fanfan <261781885@qq.com>
2023-10-20 00:23:15 +0800

    add controller for websocket

---
+++ src/main/java/com/bol/interview/mancala/controller/MancalaGameWebsocketController.java
@@ -0,0 +1,16 @@
+package com.bol.interview.mancala.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class MancalaGameWebsocketController {
+    @RequestMapping({"/","mancalagame"})
+    public String toMancalaGame(){
+        return "mancalagame";
+    }
+
+
+}
