fcdadc17c1189899b8f3292206e41890e64668b2
fanfan <261781885@qq.com>
2023-10-20 01:19:55 +0800

    fix move both 2 player house issue

--- src/main/java/com/bol/interview/mancala/rule/MoveBothPits2PlayerHouseRule.java
+++ src/main/java/com/bol/interview/mancala/rule/MoveBothPits2PlayerHouseRule.java
@@ -9,14 +9,14 @@ public class MoveBothPits2PlayerHouseRule implements GameRule{
 
     @Override
     public boolean apply(MancalaGame mancalaGame, SegmentSowResult result){
-        int lastSowIndex = result.getLastSowPitIndex();
+        int lastSowPitIdx = result.getLastSowPitIndex();
         String sowRequestPlayer = result.getSowRequestPlayer();
         String lastPitOwner = result.getLastPitOwner();
         BoardSegment playerSegment = mancalaGame.getActiveBoardSegment();
 
-        if(sowRequestPlayer.equals(lastPitOwner) && playerSegment.getStoneCntByPitIdx(lastSowIndex) == 1){
-            playerSegment.addStones2House(playerSegment.clearStonesByPitIdx(lastSowIndex));
-            int stoneNum = mancalaGame.getInactiveBoardSegment().clearStonesByPitIdx(MancalaConstants.PITS_NUM - lastSowIndex);
+        if(sowRequestPlayer.equals(lastPitOwner) && playerSegment.getStoneCntByPitIdx(lastSowPitIdx) == 1){
+            playerSegment.addStones2House(playerSegment.clearStonesByPitIdx(lastSowPitIdx));
+            int stoneNum = mancalaGame.getInactiveBoardSegment().clearStonesByPitIdx(MancalaConstants.LAST_PIT_INDEX - lastSowPitIdx);
             playerSegment.addStones2House(stoneNum);
             mancalaGame.setActiveBoardSegment(mancalaGame.getInactiveBoardSegment());
             mancalaGame.setInactiveBoardSegment(playerSegment);
