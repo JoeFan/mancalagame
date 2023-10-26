3f0637cc2722cb128eda13a1488323e2c2592dea
fanfan <261781885@qq.com>
2023-10-26 14:58:32 +0800

    remove unsued config impl

--- src/main/java/com/bol/interview/mancala/config/WebSocketConfig.java
+++ src/main/java/com/bol/interview/mancala/config/WebSocketConfig.java
@@ -2,11 +2,10 @@ package com.bol.interview.mancala.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig{
 
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
